158e64f19cfa3d525a0b2f9598fe709faba2d37c
Sumanta Das <sumantad827@gmail.com>
2022-11-19 13:16:49 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+# write a python program to input a number from user and Calculate its cube
+
+userinput = int(input("Enter A Number : "))
+
+calculatecube = userinput*userinput*userinput
+
+print('you enter : ', userinput)
+print('your Number cube is : ', calculatecube)
